2346ffba1a79fd2a021e680769fff1bc5ae8bcb2
Anderson N Freire <coolerinformatica1995@gmail.com>
2020-01-10 14:32:11 -0200

    Criando a classe Conexao e conectando com banco

--- classes/Conexao.php
+++ classes/Conexao.php
@@ -1,16 +1,42 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of Conexao
  *
  * @author andersonfreire
  */
 class Conexao {
-    //put your code here
+    
+    private $usuario;
+    private $senha;
+    private $banco;
+    private $servidor;
+    private static $pdo;
+    
+    //Construtor
+    function __construct() {
+        $this->usuario = "root";
+        $this->senha = "";
+        $this->banco = "bancoPHP";
+        $this->servidor = "localhost";
+    }
+
+    //Metodo para conectar com o banco
+    
+    function conectar()
+    {
+        try
+        {
+            if(is_null(self::$pdo))
+            {
+                self::$pdo = new PDO("mysql:host=".$this->servidor.";dbname=".$this->banco, $this->usuario, $this->senha);
+            }
+            return self::$pdo;
+        } catch (PDOException $ex) {
+            echo'Erro. '.$ex->getMessage();    
+        }
+    }
+    
+    
 }
+
